0f3a8a764f1d0247f3c1b417ec30660b0faabd98
patrickleon <leon.patrick19@gmail.com>
2017-06-26 15:59:18 -0500

    made changes

--- main.js
+++ main.js
@@ -1,5 +1,36 @@
-function name() {
-	
- }
+const heightInput = document.getElementById('heightInput');
+const characterInput = document.getElementById('characterInput');
+const button = document.getElementById('GrowYourTree');
 
- console.log ("output","*");
+
+var Tree = {
+	height: "",
+	character: ""
+
+
+};
+button.addEventListener("click", function(){
+	growTree (Tree.height, Tree.character)
+});
+
+heightInput.addEventListener ("keyup", function(){
+	Tree.height = heightInput.value
+
+})
+
+characterInput.addEventListener ("keyup", function(){
+	Tree.character = characterInput.value
+})
+
+function growTree (num, char){
+var Leaves = " ";
+	for (var i = 0; i < num; i++) {
+		Leaves +=char 
+	console.log(Leaves)
+
+
+
+
+}
+}
+
